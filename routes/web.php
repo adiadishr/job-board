f2eab29c62af79c4624d8afd8808c532c2a0659c
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-09-25 22:54:30 +0545

    replaced the existing code with the find method of the Job class in the route file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -14,7 +15,7 @@ Route::get('/jobs', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
     return view('job', [
         'job' => $job
     ]);
