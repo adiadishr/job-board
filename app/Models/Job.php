759e20ea2b504382d41874c454c394bcb5cb7b72
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-09-25 22:57:14 +0545

    handled logic for the case where a job is not found based on the id provided in the uri, removed the Arr class from the routing file

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -30,6 +30,10 @@ class Job
 
     public static function find(int $id): array
     {
-        return Arr::first(static::all(), fn($job) => $job['id'] == $id);
+        $job =  Arr::first(static::all(), fn($job) => $job['id'] == $id);
+        if (!$job) {
+            abort(404);
+        }
+        return $job;
     }
 }
